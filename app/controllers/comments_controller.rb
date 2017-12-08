ffb7dba3c9170f8aacfe71e58b5d81c7c4027e36
kaishi9255 <kaishi9255@gmail.com>
2017-12-08 14:17:42 +0900

    コメントのAjax実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,16 +25,8 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment.post, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @comment.save
+    @comments = @comment.post.comments
   end
 
   # PATCH/PUT /comments/1
@@ -55,10 +47,6 @@ class CommentsController < ApplicationController
   # DELETE /comments/1.json
   def destroy
     @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to @comment.post, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
