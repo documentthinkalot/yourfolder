b7620dec3a64f258f6b93afb22ea61bce008cbb2
kaishi9255 <kaishi9255@gmail.com>
2017-12-14 14:50:54 +0900

    アカウント情報の編集に対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_account_update_params, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :sex, :company, :circumstances, :position, :age, :number_of_employees, :place_id, :industry_id, :occupation_id])
+  end
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :sex, :company, :circumstances, :position, :age, :number_of_employees, :place_id, :industry_id, :occupation_id])
   end
   def after_sign_out_path_for(resource)
     '/users/sign_in' # サインアウト後のリダイレクト先URL
